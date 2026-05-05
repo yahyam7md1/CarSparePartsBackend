9d5c5b378b1266b31825405f5ebf20087a37e55a
Yahia bawadekji <yahya2201129@gmail.com>
2026-05-05 16:51:07 +0300

    Added inline explaintion of some files

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,6 +1,12 @@
+//This file is used to handle the request and response for the admin routes
+//the difference between admin controller and auth controller is that admin controller is used to handle the request and response for the admin routes
+//and auth controller is used to handle the request and response for the auth routes
+
 import type { NextFunction, Request, Response } from "express";
 import * as adminRepository from "../repositories/admin.repository.js";
-
+//This function is used to handle the request and response for the admin routes
+//we are passing the request, response and next function to the function
+//nextFunction is used to handle the errors, errors such as validation errors, unauthorized errors, etc.
 export async function me(req: Request, res: Response, next: NextFunction) {
   try {
     const adminId = req.adminId;
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,24 @@
+//the reason why we are importing nextfunction, request and response from express is because we are using express framework to create a server
+//so we are going to use them to handle the request and response
 import type { NextFunction, Request, Response } from "express";
+//Zod error is used to handle the validation errors
 import { ZodError } from "zod";
+//AuthEnv is used to get the authentication environment variables
+
 import type { AuthEnv } from "../config/env.js";
+//Here we are importing the auth service to handle the authentication logic
 import * as authService from "../services/auth.service.js";
+//We are importing the loginbodyschema from the schema folder to filter grabege
 import { loginBodySchema } from "../schemas/auth.schemas.js";
+//We are importing the unauthorized error to handle the unauthorized errors
 import { UnauthorizedError } from "../utils/errors.js";
 
+
+//This function is used to create the auth controller and we are passing the auth environment variables to it
 export function createAuthController(authEnv: AuthEnv) {
   return {
+    //The world "Async" is used to tell the function to return a promise, a promise is a value that is not yet known but will be in the future
+    
     async login(req: Request, res: Response, next: NextFunction) {
       try {
         const body = loginBodySchema.parse(req.body);
